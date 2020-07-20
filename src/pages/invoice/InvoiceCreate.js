0834e8166624257f0ee3bc2620b45c3f6dcc2473
Patrick <baesslerpa@gmail.com>
2020-07-20 10:30:53 +0200

    add and delete Position functions

--- src/pages/invoice/InvoiceCreate.js
+++ src/pages/invoice/InvoiceCreate.js
@@ -4,6 +4,7 @@ import {Invoice, InvoicePosition} from '../../../database/helpers/bluePrints';
 
 import InputsFromObject from '../../components/InputsFromObject';
 import { createInvoice } from '../../../database/helpers/lowDBHelpers';
+import { Button, Pane } from 'evergreen-ui';
 
 
 /**
@@ -11,14 +12,53 @@ import { createInvoice } from '../../../database/helpers/lowDBHelpers';
  */
 const InvoiceCreate = () => {
 
+  /**
+   * Invoice state
+   */
   const [invoice, setInvoice] = useState(new Invoice())
 
+  /**
+   * All positions for the Invoice
+   */
+  const [positions, setPositions] = useState([])
+
+  /**
+   * Create a new Position Object and put it in positions
+   */
+  const createNewPosition = () => {
+    setPositions([...positions, new InvoicePosition()])
+    console.log(positions)
+  }
+
+/**
+ * Delete one Position Item in positions
+ * @param {id} id from Item to delete
+ */
+  const deletePosition = (id) => {
+    setPositions(positions.filter(position => position.id != id))
+  }
 
   return ( 
     <div>
       { invoice && <InputsFromObject obj={invoice} change={setInvoice}/>}
+      <ArrayToFields arr={positions} change={setPositions} del={deletePosition}/>
+      <Button onClick={createNewPosition}>new Position</Button>
     </div> 
   )
 }
  
+const ArrayToFields = ({arr, change, del}) => {
+
+  return (
+    <Pane background="tint2" marginBottom="16">
+      {arr.map(item => (
+        <>
+          <Button onClick={(e) => del(item.id)}>Delete</Button>
+          <InputsFromObject obj={item} change={change}/>
+        </>
+      ) )}
+    </Pane>
+  )
+}
+
 export default InvoiceCreate;
