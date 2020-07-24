84b737d80f6ebe10785e5d465fbf7368393827e6
Patrick <baesslerpa@gmail.com>
2020-07-24 15:56:51 +0200

    implementing basic ordere Function

--- src/pages/customer/CustomerCreate.js
+++ src/pages/customer/CustomerCreate.js
@@ -14,14 +14,29 @@ const CustomerCreate = () => {
   const [customer, setCustomer] = useState(new Customer())
 
   console.log(customer);
-  const saveNewCustomer = () => {
-    createCustomer(customer)
+
+  const saveNewCustomer = () => createCustomer(customer)
+
+
+  const orderFunction = (fields) => {
+
+    const customOrder = [
+      "id",
+      "name",
+      "slug",
+      "adress",
+      "defaultPrice"
+    ]
+
+    const orderedFields = customOrder.map((key, i) => fields.filter(field => field.key == key)[0])
+
+    return orderedFields
   }
 
   return ( 
     <div>
       <h1>CustomerCreate</h1>
-      { customer && <InputsFromObject obj={customer} change={setCustomer}/>}
+      { customer && <InputsFromObject obj={customer} change={setCustomer} orderFunction={orderFunction}/>}
       <button onClick={saveNewCustomer}>Save</button>
     </div> 
   );
