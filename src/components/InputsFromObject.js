05a0d270ccf8e337b1023dbba2b2ef499740a3c1
Patrick <baesslerpa@gmail.com>
2020-07-24 15:27:49 +0200

    adding changeHandler Consturktor function

--- src/components/InputsFromObject.js
+++ src/components/InputsFromObject.js
@@ -5,33 +5,40 @@ import CustomerComboBox from './inputCombonents/CustomerComboBox';
 import AdressInputs from './inputCombonents/AdressInputs';
 import slugify from 'slugify';
 
+
+/**
+ * Input change Handler to update contents
+ * @param {sting} value 
+ * @param {*} key name of the Object Key
+ * @param {*} parent parent element for subElements
+ */
+
+const changeConstructor = (change, obj) => {
+    return (value, key, parent) => {
+      switch (parent) {
+        case 'name': 
+          console.log(value);
+          return change({...obj, name: value, slug: slugify(value)})
+  
+        case 'adress':
+          return change({...obj, adress: {...obj.adress, [key]: value}})
+      
+        default:
+          change({...obj, [key]: value})
+          break;
+      } 
+    }
+}
+
+
 /**
  * Strips an Object into Inputs
  */
 
-const InputsFromObject = ({obj, change}) => {
-
-  /**
-   * Input change Handler
-   * @param {sting} value 
-   * @param {*} key name of the Object Key
-   * @param {*} parent parent element for subElements
-   */
-  let onChangeHandler = (value,key,parent) => {
-    switch (parent) {
-      case 'name': 
-        console.log(value);
-        return change({...obj, name: value, slug: slugify(value)})
-
-      case 'adress':
-        return change({...obj, adress: {...obj.adress, [key]: value}})
-    
-      default:
-        change({...obj, [key]: value})
-        break;
-    } 
-  }
+const InputsFromObject = ({obj, change, orderFunction}) => {
   
+  const onChangeHandler = changeConstructor(change, obj)
+
   /**
    * Make each key off Object to a input field
    */
@@ -68,6 +75,12 @@ const InputsFromObject = ({obj, change}) => {
     }
   }
     )
+
+  const reorderFields = () => {
+    orderFunction()
+  }
+
+  console.log(fields);
 
   // Render 
   return ( 
